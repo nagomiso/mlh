0c932521f455c6e26600b209f2d55f28658caddc
nagomiso <daiseigi.giants@gmail.com>
2014-07-09 00:53:56 +0900

    子ノード実装

--- randomForest/src/nagomiso/study/Tree.java
+++ randomForest/src/nagomiso/study/Tree.java
@@ -1,5 +1,17 @@
 package nagomiso.study;
 
 public class Tree {
-
+	private Tree leftChildNode;
+	private Tree rightChildNode;
+	
+	
+	public Tree getLeftChildNode() {
+		return leftChildNode;
+	}
+	
+	public Tree getRightChildNode() {
+		return rightChildNode;
+	}
+	
+	
 }
