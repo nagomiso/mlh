55f18ea0dd6b94d952ce8e8102f9e82be38ee3f1
nagomiso <daiseigi.giants@gmail.com>
2014-07-11 00:40:36 +0900

    Nodeの構造を記述

--- randomForest/src/nagomiso/study/Node.java
+++ randomForest/src/nagomiso/study/Node.java
@@ -1,5 +1,53 @@
 package nagomiso.study;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Node {
+	private int dimension;
+	private double threshold;
+	private double informationGain;
+	private Node parent;
+	private List<Node> children;
+
+
+	public Node() {
+		dimension = 0;
+		threshold = 0d;
+		informationGain = 0d;
+		children = new LinkedList<Node>();
+	}
+	
+	public Node(Node parent){
+		dimension = 0;
+		threshold = 0d;
+		informationGain = 0d;
+		children = new LinkedList<Node>();
+		this.parent = parent;
+	}
+	
+	public void add(Node child){
+		this.children.add(child);
+	}
+
+	public int getDimension() {
+		return dimension;
+	}
+
+	public double getThreshold() {
+		return threshold;
+	}
+
+	public Node getParent() {
+		return parent;
+	}
+
+	public List<Node> getChildren() {
+		return children;
+	}
+
+	public double getInformationGain() {
+		return informationGain;
+	}
 
 }
